3b6fd10e5d0b50f919837cf97b730e0ed47c1db9
masterprocess <misterrackley@gmail.com>
2018-03-14 22:07:10 -0400

    expanded functionality, will show pictures

--- app/src/main/java/com/example/masterprocess/listapp/MainActivity.java
+++ app/src/main/java/com/example/masterprocess/listapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.masterprocess.listapp;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -26,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
 
         ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions);
         myListView.setAdapter(itemAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent showDetailActivity = new Intent(getApplicationContext(), DetailActivity.class);
+                showDetailActivity.putExtra("com.example.masterprocess.ITEM_INDEX", position);
+                startActivity(showDetailActivity);
+            }
+        });
 
     }
 }
